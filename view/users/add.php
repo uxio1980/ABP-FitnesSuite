c4752d6d7c9a0320e37730cc8a6655e8ed45a7cd
Sandra <spgiraldez@esei.uvigo.es>
2017-11-20 21:15:25 +0100

    Resolve issues.
    Resolved problems when registering a user and when confirming it.

--- view/users/add.php
+++ view/users/add.php
@@ -3,11 +3,10 @@
 
 require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
-
 $errors = $view->getVariable("errors");
-
+$loginerr = $view->getVariable("loginerrors");
+$user = $view->getVariable("user");
 $view->setVariable("title", "Add User");
-
 ?>
 
 <main id="main-content">
@@ -17,21 +16,23 @@ $view->setVariable("title", "Add User");
 
             <strong><?= i18n("Add User") ?></strong>
 
-            <label for="form-field"><?= i18n("Username") ;?></label>
-            <input type="text" name="login" value="" placeholder=""/>
+            <?= isset($loginerr["general"])?$loginerr["general"]:"" ?>
 
+            <label for="form-field"><?= i18n("Username") ;?></label>
+            <input type="text" name="login" value="<?=$user->getLogin() ?>" placeholder=""/>
+            <?= isset($errors["register-login"])?$errors["register-login"]:"" ?>
 
             <label for="name-field"><?= i18n("Name") ?></label>
-            <input type="text" name="name" value="" placeholder=""/>
-
+            <input type="text" name="name" value="<?=$user->getName() ?>" placeholder=""/>
+            <?= isset($errors["register-name"])?$errors["register-name"]:"" ?>
 
             <label for="form-field"><?= i18n("Password") ?></label>
             <input type="password" name="password" placeholder=""/>
-
+            <?= isset($errors["register-password"])?$errors["register-password"]:"" ?>
 
             <label for="form-field"><?= i18n("Email") ?></label>
-            <input type="text" name="email" value="" placeholder=""/>
-
+            <input type="text" name="email" value="<?=$user->getEmail() ?>" placeholder=""/>
+            <?= isset($errors["register-email"])?$errors["register-email"]:"" ?>
 
             <input type="submit" name="submit" value="<?= i18n("Register") ?>"/>
         </form>
